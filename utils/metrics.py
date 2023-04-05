0e84b6c1fc6bdcd2998c0efab9ade0da50236d44
Nguyen Anh Khoa <anhkhoa.nguyen@ru.nl>
2023-04-05 20:46:31 +0200

    Add minkowski, cosine, jaccard, chebyshev, and hamming distance metrics

---
+++ utils/metrics.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def minkowski_distance(x, y, p=2):
+     return np.sum(np.abs(x - y) ** p) ** (1 / p)
+
+def cosine_similarity(x, y, distance=False):
+     if distance:
+          return 1 - np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
+     else:
+          return np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
+     
+def jaccard_similarity(x, y, distance=False):
+     intersection = np.sum(np.minimum(x, y))
+     union = np.sum(np.maximum(x, y))
+     if distance:
+          return 1 - intersection / union
+     else:
+          return intersection / union
+     
+def chebyshev_distance(x, y):
+     return np.max(np.abs(x - y))
+     
+def hamming_distance(x, y):
+     return np.sum(np.abs(x - y)) / len(x)
+
+
+    
