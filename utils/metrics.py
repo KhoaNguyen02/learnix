8f3d0624494433d22ee80a2d98c56fdeb8f040d1
Nguyen Anh Khoa <anhkhoa.nguyen@ru.nl>
2023-04-06 02:41:43 +0200

    Add distance metrics factory

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+def get_dist(x, y, type='euclidean', p=2):
+     dist_dict = {'euclidean': minkowski_distance(x, y, p=2), 'manhattan': minkowski_distance(x, y, p=1),
+                  'minkowski': minkowski_distance(x, y, p=p), 'cosine': cosine_similarity(x, y, distance=True),
+                    'hamming': hamming_distance(x, y), 'chebyshev': chebyshev_distance(x, y),
+                    'jaccard': jaccard_similarity(x, y, distance=True)}
+     return dist_dict[type]
+
 def minkowski_distance(x, y, p=2):
      return np.sum(np.abs(x - y) ** p) ** (1 / p)
 
