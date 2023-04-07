6d54c6ca7e402bcd27f49034d3edd2f07865df2f
Nguyen Anh Khoa <anhkhoa.nguyen@ru.nl>
2023-04-07 22:48:14 +0200

    Add linear kernel, polynomial kernel, rbf kernel, and sigmoid kernel

--- utils/misc.py
+++ utils/misc.py
@@ -63,3 +63,57 @@ def cross_entropy_loss(y_true, y_pred, grad=False):
         return y_pred - y_true
     else:
         return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+    
+def linear_kernel(x1, x2):
+    """Linear kernel.
+
+    Args:
+        x1 (np.array): of shape (n_features, ) containing the first sample.
+        x2 (np.array): of shape (n_features, ) containing the second sample.
+
+    Returns:
+        float: the kernel value.
+    """
+    return np.dot(x1, x2)
+
+def polynomial_kernel(x1, x2, p=2, gamma=1, coef=0):
+    """Polynomial kernel.
+
+    Args:
+        x1 (np.array): of shape (n_features, ) containing the first sample.
+        x2 (np.array): of shape (n_features, ) containing the second sample.
+        p (int, optional): degree of the polynomial. Defaults to 2.
+        gamma (float, optional): kernel coefficient. Defaults to 1.
+        coef (float, optional): independent term. Defaults to 0.
+
+    Returns:
+        float: the kernel value.
+    """
+    return (gamma * np.dot(x1, x2) + coef) ** p
+
+def rbf_kernel(x1, x2, gamma=1):
+    """Radial basis function kernel.
+
+    Args:
+        x1 (np.array): of shape (n_features, ) containing the first sample.
+        x2 (np.array): of shape (n_features, ) containing the second sample.
+        gamma (float, optional): kernel coefficient. Defaults to 1.
+
+    Returns:
+        float: the kernel value.
+    """
+    return np.exp(-gamma * np.sum((x1 - x2) ** 2))
+
+def sigmoid_kernel(x1, x2, gamma=1, coef=0):
+    """Sigmoid kernel.
+
+    Args:
+        x1 (np.array): of shape (n_features, ) containing the first sample.
+        x2 (np.array): of shape (n_features, ) containing the second sample.
+        gamma (float, optional): kernel coefficient. Defaults to 1.
+        coef (float, optional): independent term. Defaults to 0.
+
+    Returns:
+        float: the kernel value.
+    """
+    return np.tanh(gamma * np.dot(x1, x2) + coef)
